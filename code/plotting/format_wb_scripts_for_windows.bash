60020ecd84b7b9c27d9775504f6cb1def601c855
Garren Gaut <gbius06@gmail.com>
2018-07-22 10:33:57 -0700

    trying to fix windows bugs

--- code/plotting/format_wb_scripts_for_windows.bash
+++ code/plotting/format_wb_scripts_for_windows.bash
@@ -1,9 +1,9 @@
-#!/usr/bin/bash
+#!/usr/bin/bash"
 
-sed -i 's/\r$//' wb_cont_format.bash
-sed -i 's/\r$//' wb_cont_format_mdl2.bash
-sed -i 's/\r$//' wb_cont_to_image_fixed_mdl2.bash
-sed -i 's/\r$//' wb_cont_format_fixed.bash
-sed -i 's/\r$//' wb_cont_format_fixed_mdl2.bash
-sed -i 's/\r$//' wb_cont_to_image_fixed.bash
-sed -i 's/\r$//' wb_global_variables.bash
+sed -i 's/\r$//' "wb_cont_format.bash"
+sed -i 's/\r$//' "wb_cont_format_mdl2.bash"
+sed -i 's/\r$//' "wb_cont_to_image_fixed_mdl2.bash"
+sed -i 's/\r$//' "wb_cont_format_fixed.bash"
+sed -i 's/\r$//' "wb_cont_format_fixed_mdl2.bash"
+sed -i 's/\r$//' "wb_cont_to_image_fixed.bash"
+sed -i 's/\r$//' "wb_global_variables.bash"
