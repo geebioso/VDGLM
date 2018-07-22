e89304c241560ada5502686728ee4e52c7e6d988
Garren Gaut <gbius06@gmail.com>
2018-07-22 10:37:14 -0700

    trying to fix windows bugs

--- code/plotting/wb_global_variables.bash
+++ code/plotting/wb_global_variables.bash
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
-MAIN_FILE_DIRECTORY="/Users/Garren/Dropbox/FMRI/Projects/varianceGLM/ROI2NIfTI" 
-IMAGE_DIRECTORY="/Users/Garren/Dropbox/FMRI/Projects/varianceGLM/images" 
+#MAIN_FILE_DIRECTORY="/Users/Garren/Dropbox/FMRI/Projects/varianceGLM/ROI2NIfTI" 
+#IMAGE_DIRECTORY="/Users/Garren/Dropbox/FMRI/Projects/varianceGLM/images" 
 
+MAIN_FILE_DIRECTORY="/Users/msteyver/VDGLM/ROI2NIfTI" 
+IMAGE_DIRECTORY="/Users/msteyver/VDGLM/images"
